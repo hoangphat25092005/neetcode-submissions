8ac23a19db7640731897f2619ee68e2784b9bd84
HoangPhatCs <87558213+hoangphat25092005@users.noreply.github.com>
2026-09-13 07:58:59 +0700

    Add: find-peak-element - submission-6

---
+++ Data Structures & Algorithms/find-peak-element/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        # only one element
+        if n == 1:
+            return 0
+    
+        # Check middle elements
+        for i in range(1, n - 1):
+            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+                return i
+    
+        # Check first element
+        if nums[0] > nums[1]:
+            return 0
+    
+        # Check last element
+        if nums[n - 1] > nums[n - 2]:
+            return n - 1
+    
+        return -1
